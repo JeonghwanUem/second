285fc47aac674165035b68d03393088b889050e1
rightlight <31889234+rightlight@users.noreply.github.com>
2017-09-15 20:24:09 +0900

    Add files via upload

--- ForTest8.java
+++ ForTest8.java
@@ -4,11 +4,11 @@ class ForTest8
 	{
 		int i,j,z;
 	
-	/*	1 2 3 4 5 
+	/*	0 1 2 3 4 
 		1 2 3 4 5 
-		1 2 3 4 5 
-		1 2 3 4 5
-		1 2 3 4 5 */
+		2 3 4 5 6 
+		3 4 5 6 7 
+		4 5 6 7 8  */
 		
 		for (i=0; i<5;i++ )
 		{for (j=i;j<i+5 ;j++ )
@@ -28,7 +28,7 @@ class ForTest8
 		for (i=1; i<6;i++ )
 		{for (j=0;j<i ;j++ )
 		{
-			System.out.print(j+" ");
+			System.out.print(j+" ");	
 		}
 		System.out.println();
 		}
@@ -60,7 +60,7 @@ class ForTest8
 		
 		for ( i = 0; i <= 4; i++ ) 
 		{
-			for (j = i; j <= i+4;j++ )
+			for (j = 0; j <= 4;j++ )
 			{
 				
 			
@@ -72,10 +72,10 @@ class ForTest8
 		System.out.println("-----------------");
 
 		  /*1
-		  2 2
-		3 3	3	
-	  4 4 4 4 
-	5 5 5 5 5//
+		  1 2
+		1 2	3	
+	  1 2 3 4 
+	1 2 3 4 5//
 	
 	*/
 		
@@ -98,16 +98,45 @@ class ForTest8
 	3 3 3
 	2 2 2 2
 	1 1 1 1 1*/
-			for (i =5; i<=8 ;i++ )
-			{for (j=5; j<=i&&j>0;j-- )
-			{System.out.print(j+"  ");
+			for (i =5; i>=0 ;i-- )
+			{for (j=5; j>=i ;j-- )
+			{System.out.print(i+"  ");
 			}
 			System.out.println();//여기 ln안쓰니깐 오류난다
 			}
 			System.out.print("----------------- ");
 
-	
-	
+
+/*
+****1
+***22
+**333
+*4444
+55555
+*/
+
+for (i=0;i<=5 ;i++ )
+{for (z=4;z>=i ;z-- )
+{System.out.print("* ");
+}
+for (j=1; j<=i ;j++ )
+{System.out.print(i+" ");
+}
+System.out.println();
+}
+
+
+for (z=0;z>=5 ;z-- )
+{if (z>4)
+	System.out.print("* ");
+}
+for (j=1; j<=z ;j++ )
+{System.out.print(i+" ");
+}
+System.out.println();
+
+
+
 	/*
 	0 1 2 3 4 
 	0 1 2 3 4
@@ -138,7 +167,7 @@ class ForTest8
 	4 4
 	3 3 3
 	2 2 2 2
-	1 1 1 1 1
+	1 1 1 1 1//
 	---------------
 			1
 		  2 2
@@ -148,7 +177,14 @@ class ForTest8
 	
 	*/
 	
-	
-	}
+  
+		  
 
-}
+ 	
+ 			}  
+ 		}  
+
+
+	
+
+
---
+++ TestPre0915.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class TestPre0915
+{
+  	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		int i;
+		String str="메뉴를 선택했습니다.";
+		while (true)
+		{
+		System.out.print("[성적 관리 프로그램]\n1.입력\n2.삭제\n3.수정\n4.검색\n선택 > ");
+		
+		i= scan.nextInt();
+		
+		if (i>4)
+		{System.out.println("잘못입력했습니다"); break; 
+		}
+		
+		switch (i)
+		{
+		case 1: System.out.println("입력"+str); break;
+		
+		case 2: System.out.println("삭제"+str); break;
+		
+		case 3: System.out.println("수정"+str); break;
+		
+		case 4: System.out.println("검색"+str); break;
+
+		default : System.out.println("프로그램을 종료합니다"); return;
+		}
+		}
+
+		System.out.println(" "); 
+		
+	}
+}
+
+/*
+[성적 관리 프로그램]
+1.입력
+2.삭제
+3.수정
+4.종료
+선택 > _1
+
+입력메뉴 선택했습니다. 
+
+0입력하면==>프로그램을 종료합니다==>끝
+
+5라고 입력하면 잘못된 번호입니다. ==>다시 메인으로
+
+
+while 을 사용해서if switch*/
